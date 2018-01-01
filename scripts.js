22f885743903b29cdd733a621a2dc74c5d147824
Jeffrey  McCommas <jeffmccommas@gmail.com>
2018-01-01 10:37:22 -0700

    table cells loop

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 /**
  * Created by jmccommas on 1/1/18 9:51 AM*/
+
+let  output =document.getElementById('output');
+let html='<table>';
+for (let row=0;row<10; row++){
+	
+	html+='<tr>';
+	for (let col=0;col<2;col++){
+		html+='<td> cell='+col+'</td>'
+		html+='<tr>';
+		
+	}
+	html+='</tr>'
+	
+}
+html+='</table'
+output.innerHTML=html
+
